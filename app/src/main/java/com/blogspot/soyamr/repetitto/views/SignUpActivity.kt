a96290478a2aeba45a75f81400e80cea1b7408bc
Amr AboElhamd <amr.abuelhamd@gmail.com>
2020-02-06 14:29:41 +0700

    - added constraint layout that hides if swich is false and appear otherwise

--- app/src/main/java/com/blogspot/soyamr/repetitto/views/SignUpActivity.kt
+++ app/src/main/java/com/blogspot/soyamr/repetitto/views/SignUpActivity.kt
@@ -3,7 +3,9 @@ package com.blogspot.soyamr.repetitto.views;
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Switch
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.blogspot.soyamr.repetitto.R
 
 class SignUpActivity : AppCompatActivity() {
@@ -17,6 +19,17 @@ class SignUpActivity : AppCompatActivity() {
         //also receive his data and save it in abstract static class
         val profileIntent = Intent(this, RecyclerViewActivity::class.java)
         startActivity(profileIntent)
+    }
+
+    fun showOtherPrefs(view: View) {
+        val constraintLayout: ConstraintLayout = this.findViewById(R.id.constraintLayout)
+        val switch: Switch = this.findViewById(R.id.switch1)
+
+        if (switch.isChecked)
+            constraintLayout.visibility = View.VISIBLE
+        else
+            constraintLayout.visibility = View.GONE
+
     }
 
 }
